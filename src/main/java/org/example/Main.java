1e2e48651e7f7c2c09f688b91a7c097976b6e5d3
Muhammed Hamza Toptancı <toptanci.hamza@gmail.com>
2024-07-16 15:29:12 +0300

    Solved.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,51 @@
 package org.example;
 
 
+import org.example.entity.Employee;
+
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        List<Employee> employees = new LinkedList<>();
+        employees.add(new Employee(1, "John", "Doe"));
+        employees.add(new Employee(2, "Jane", "Doe"));
+        employees.add(new Employee(3, "Ece", "Doe"));
+        employees.add(new Employee(4, "Johnnie", "Doe"));
+        employees.add(new Employee(3, "Ece", "Doe"));
+
+        System.out.println("*********************************");
+        System.out.println(findUniques(employees));
+        System.out.println(findDuplicates(employees));
+        System.out.println(removeDuplicates(employees));
+
+    }
+
+    public static List<Employee> findDuplicates(List<Employee> employeeList) {
+        List<Employee> unique = new ArrayList<>();
+        List<Employee> duplicated = new ArrayList<>();
+        for (Employee employee : employeeList) {
+            if (unique.contains(employee)) duplicated.add(employee);
+            else unique.add(employee);
+        }
+        return duplicated;
+    }
+
+    public static Map<Integer, Employee> findUniques(List<Employee> employeeList) {
+        Map<Integer, Employee> employees = new HashMap<>();
+        for (Employee emp : employeeList) {
+            if (emp != null) employees.put(emp.getId(), emp);
+        }
+        return employees;
+    }
+
+    public static List<Employee> removeDuplicates(List<Employee> employees) {
+        List<Employee> duplicates = findDuplicates(employees);
+        Map<Integer, Employee> uniques = findUniques(employees);
+        List<Employee> onlyUnique = new LinkedList<>(uniques.values());
+        onlyUnique.removeAll(duplicates);
+        return onlyUnique;
     }
 }
