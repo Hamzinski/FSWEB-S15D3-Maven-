fa44ed09e16c26395474637960e3be645d378ea5
Muhammed Hamza Toptancı <toptanci.hamza@gmail.com>
2024-07-16 15:30:07 +0300

    Solved.

--- src/main/java/org/example/entity/Employee.java
+++ src/main/java/org/example/entity/Employee.java
@@ -7,7 +7,6 @@ public class Employee {
     private String firstname;
     private String lastname;
 
-
     public Employee(int id, String firstname, String lastname) {
         this.id = id;
         this.firstname = firstname;
